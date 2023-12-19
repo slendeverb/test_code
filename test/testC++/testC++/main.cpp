f0c1e861c0bf75c1b8bb672928f180b905772f91
stwings <571641990@qq.com>
2023-12-19 18:40:38 +0800

    update

--- test/testC++/testC++/main.cpp
+++ test/testC++/testC++/main.cpp
@@ -1,39 +1,7 @@
-#include "HanoiTower.h"
+#include <iostream>
 
 int main()
 {
-	HanoiTower hanoiTower;
-	int userKey;
-	do
-	{
-		system("cls");
-		hanoiTower.showMenu();
-		std::cout << "请选择: ";
-		std::cin >> userKey;
-		HanoiTower::Key key{ static_cast<HanoiTower::Key>(userKey) };
-		/*using enum HanoiTower::Key;*/
-		using Key = HanoiTower::Key;
-		switch (key)
-		{
-		case Key::START:
-			hanoiTower.startGame();
-			break;
-		case Key::RECORD:
-			hanoiTower.showRecord();
-			break;
-		case Key::INSTRUCTION:
-			hanoiTower.showInstruction();
-			break;
-		case Key::EXIT:
-			std::cout << "退出游戏!\n" << std::endl;
-			break;
-		default:
-			std::cout << "输入无效!\n" << std::endl;
-			system("pause");
-			break;
-		}
-	} while (userKey != 0);
 
-	system("pause");
 	return 0;
 }
