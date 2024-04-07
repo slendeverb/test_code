4e6841204c7eb72d63cfe4e4f0d5066e4c508870
stwings <571641990@qq.com>
2024-04-07 19:49:21 +0800

    update

--- test/testC++/testC++/main.cpp
+++ test/testC++/testC++/main.cpp
@@ -1,11 +1,99 @@
-#include "C:/msys64/mingw64/include/c++/13.2.0/x86_64-w64-mingw32/bits/stdc++.h"
+#include <algorithm>
+#include <any>
+#include <array>
+#include <atomic>
+#include <barrier>
+#include <bit>
+#include <bitset>
+#include <charconv>
+#include <chrono>
+#include <cmath>
+#include <compare>
+#include <complex>
+#include <concepts>
+#include <condition_variable>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <latch>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <new>
+#include <numbers>
+#include <numeric>
+#include <optional>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <ratio>
+#include <regex>
+#include <semaphore>
+#include <set>
+#include <source_location>
+#include <span>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <variant>
+#include <vector>
+#include <version>
+#include <windows.h>
+using namespace std;
+
+
 
 void solve() {
-
+    
 }
 
-int main()
-{
-	solve();
+int main() {
+    std::ifstream in{ "./in.txt" };
+    std::streambuf* oldIn{ std::cin.rdbuf(in.rdbuf()) };
+    std::ofstream out{ "./out.txt" };
+    std::streambuf* oldOut{ std::cout.rdbuf(out.rdbuf()) };
+    std::ofstream err{ "./err.txt" };
+    std::streambuf* oldErr{ std::cerr.rdbuf(err.rdbuf()) };
+    std::streambuf* oldLog{ std::clog.rdbuf(err.rdbuf()) };
+
+    clock_t startTime{ clock() };
+    solve();
+    clock_t endTime{ clock() };
+    std::cout << "\ntime cost: " << endTime - startTime << std::endl;
+
+    in.close();
+    std::cin.rdbuf(oldIn);
+    out.close();
+    std::cout.rdbuf(oldOut);
+    err.close();
+    std::cerr.rdbuf(oldErr);
+    std::clog.rdbuf(oldLog);
 	return 0;
 }
