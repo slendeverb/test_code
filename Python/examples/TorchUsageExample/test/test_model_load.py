a7f9ca1da8b360675eb6a2c6fcbc97e428e5bfba
stwings <571641990@qq.com>
2024-04-26 23:40:09 +0800

    update

--- Python/examples/TorchUsageExample/test/test_model_load.py
+++ Python/examples/TorchUsageExample/test/test_model_load.py
@@ -8,12 +8,9 @@ model = torch.load("../models/vgg16/not_trained/vgg16_method1.pth")
 # 读取方式2 加载字典
 # model = torchvision.models.vgg16(weights=None)
 # model.load_state_dict(torch.load("../models/vgg16/not_trained/vgg16_method2.pth"))
-# model = torch.load("../models/vgg16/not_trained/vgg16_method2.pth")
 
 # 陷阱
-
 # from test_model_save import *
-
 # model = torch.load("../models/tudui/not_trained/tudui_method1.pth")
 
 print(model)
