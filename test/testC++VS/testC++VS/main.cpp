febb6cfa9db3bdd71c6ebf01e940c4f5ddddcf29
stwings <571641990@qq.com>
2024-04-26 20:56:49 +0800

    update

--- test/testC++VS/testC++VS/main.cpp
+++ test/testC++VS/testC++VS/main.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 
 void solve() {
-    std::string input_path{"./in.txt"};
-    std::ifstream in{input_path};
+    std::string input_path{ "./in.txt" };
+    std::ifstream in{ input_path };
     std::string a, b;
     in >> a >> b;
     size_t m = a.size(), n = b.size();
@@ -20,7 +20,8 @@ void solve() {
         for (size_t j = 1; j <= n; j++) {
             if (a[i - 1] != b[j - 1]) {
                 dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
-            } else {
+            }
+            else {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             }
         }
@@ -31,16 +32,18 @@ void solve() {
         for (size_t j = n; j >= 1;) {
             if (dp[i][j] == dp[i][j - 1]) {
                 j--;
-            } else if (dp[i][j] == dp[i - 1][j]) {
+            }
+            else if (dp[i][j] == dp[i - 1][j]) {
                 i--;
-            } else {
+            }
+            else {
                 i--, j--;
                 LCS_ans.push_back(a[i]);
             }
         }
     }
-    std::string output_path{"./out.txt"};
-    std::ofstream out{output_path};
+    std::string output_path{ "./out.txt" };
+    std::ofstream out{ output_path };
     out << LCS_length << "\n"
         << std::string(LCS_ans.rbegin(), LCS_ans.rend()) << std::endl;
 }
