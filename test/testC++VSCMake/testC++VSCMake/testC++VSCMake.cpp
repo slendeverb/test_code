61c0e0239a6dad0b5ccccfe173e22a06d262b348
stwings <571641990@qq.com>
2024-05-07 21:10:00 +0800

    update

---
+++ test/testC++VSCMake/testC++VSCMake/testC++VSCMake.cpp
@@ -0,0 +1,14 @@
+﻿// testC++VSCMake.cpp: 定义应用程序的入口点。
+//
+
+#include "testC++VSCMake.h"
+
+void solve() {
+	std::cout << torch::cuda::is_available() << std::endl;
+}
+
+int main()
+{
+	solve();
+	return 0;
+}
---
+++ test/testC++VSCMake/testC++VSCMake/testC++VSCMake.h
@@ -0,0 +1,9 @@
+﻿// testC++VSCMake.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <torch/torch.h>
+
+// TODO: 在此处引用程序需要的其他标头。
