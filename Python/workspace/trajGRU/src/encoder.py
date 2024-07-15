f64a41e00001dfdb06a5e2c4bbe3f7cd09e7df9e
stwings <571641990@qq.com>
2024-07-15 11:34:47 +0800

    update

--- Python/workspace/trajGRU/src/encoder.py
+++ Python/workspace/trajGRU/src/encoder.py
@@ -10,14 +10,16 @@ class Encoder(nn.Module):
 
         self.blocks = len(subnets)
         for index, (params, rnn) in enumerate(zip(subnets, rnns), 1):
-            setattr(self, 'stage' + str(index), make_layers(params))
-            setattr(self, 'rnn' + str(index), rnn)
+            setattr(self, "stage" + str(index), make_layers(params))
+            setattr(self, "rnn" + str(index), rnn)
 
     def forward_by_stage(self, input, subnet, rnn):
         seq_number, batch_size, input_channel, height, width = input.size()
         input = torch.reshape(input, (-1, input_channel, height, width))
         input = subnet(input)
-        input = torch.reshape(input, (seq_number, batch_size, input.size(1), input.size(2), input.size(3)))
+        input = torch.reshape(
+            input, (seq_number, batch_size, input.size(1), input.size(2), input.size(3))
+        )
 
         outputs_stage, state_stage = rnn(input, None)
         return outputs_stage, state_stage
@@ -26,11 +28,12 @@ class Encoder(nn.Module):
 
     def forward(self, input):
         hidden_states = []
-        logging.debug(input.size())
+        # logging.debug(input.size())
 
         for i in range(1, self.blocks + 1):
-            input, state_stage = self.forward_by_stage(input, getattr(self, 'stage' + str(i)),
-                                                       getattr(self, 'rnn' + str(i)))
+            input, state_stage = self.forward_by_stage(
+                input, getattr(self, "stage" + str(i)), getattr(self, "rnn" + str(i))
+            )
             # print(f'stage {i} shape {input.shape} rnn {i} shape {state_stage.shape}')
             hidden_states.append(state_stage)
 
