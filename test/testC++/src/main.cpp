2b2ef7bd594016706532f6ab377fb374bdb9cd47
stwings <571641990@qq.com>
2024-09-29 21:12:22 +0800

    update

--- test/testC++/src/main.cpp
+++ test/testC++/src/main.cpp
@@ -2,7 +2,11 @@
 
 int main(int argc,char** argv) {
     cv::Mat image=cv::imread("C:/Users/slendeverb/Pictures/Saved Pictures/117541346_p0_master1200.jpg");
-    cv::imshow("image",image);
+    cv::Mat resized_image;
+    int resized_cols=1080*0.9;
+    int resized_rows=;
+    cv::resize(image,resized_image,cv::Size(resized_rows,resized_cols));
+    cv::imshow("image",resized_image);
     cv::waitKey();
 
     torch::Tensor tensor=torch::rand({10,10});
