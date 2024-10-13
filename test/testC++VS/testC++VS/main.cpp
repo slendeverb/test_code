28731a641c9aaac202e56b64f00f2ac8131fb7ff
stwings <571641990@qq.com>
2024-10-13 17:25:21 +0800

    update

--- test/testC++VS/testC++VS/main.cpp
+++ test/testC++VS/testC++VS/main.cpp
@@ -1,7 +1,23 @@
-#include <bits/stdc++.h>
+import std;
+
+class Solution {
+public:
+	int duplicateNumbersXOR(std::vector<int>& nums) {
+		std::unordered_set<int, int> s;
+		int ans = 0;
+		for (const auto& x : nums) {
+			if (s.contains(x)) {
+				ans ^= x;
+			}
+			else {
+				s.insert(x);
+			}
+		}
+		return ans;
+	}
+};
 
 int main(int argc, char** argv) {
-	std::string s;
-	std::cin >> s;
-	std::cout << s;
+	std::vector<int> nums{ 1,2,1,3 };
+	std::cout << std::format("{}\n", Solution{}.duplicateNumbersXOR(nums));
 }
