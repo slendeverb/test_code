b464414ce19900d6ffdf3806ddb42cc444d10432
stwings <571641990@qq.com>
2024-10-19 14:01:54 +0800

    update

--- test/testC++/src/main.cpp
+++ test/testC++/src/main.cpp
@@ -1,5 +1,27 @@
 #include "header.h"
 
+class Solution {
+    public:
+    int dulplicateNumbersXOR(std::vector<int> &&nums) {
+        std::unordered_set<int> set;
+        int answer = 0;
+        for(const auto &num : nums) {
+            if(set.contains(num)) {
+                answer^=num;
+            } else {
+                set.insert(num);
+            }
+        }
+        return answer;
+    }
+};
+
 int main(int argc,char** argv) {
-    printf("Hello World\n");
+    auto start=std::chrono::high_resolution_clock::now();
+    auto obj=Solution{};
+    auto ans=obj.dulplicateNumbersXOR({1,1,2,3});
+    std::println("{}",ans);
+    auto end=std::chrono::high_resolution_clock::now();
+    auto duration=std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    std::println("Time elapsed: {}",duration);
 }
