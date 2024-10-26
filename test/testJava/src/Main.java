835958693a6213609111e62ce988e69e6ab40ed9
stwings <571641990@qq.com>
2024-10-27 02:02:49 +0800

    update

--- test/testJava/src/Main.java
+++ test/testJava/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         BigInteger bigInteger=new BigInteger("1");
-        System.out.println(bigInteger.shiftLeft(10000000));
+        System.out.println(bigInteger.shiftLeft(1000000));
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
         System.out.println(elapsedTime);
