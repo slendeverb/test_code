d6f23dbb3f5d7b8d3af2bf2321246391b80422c4
stwings <571641990@qq.com>
2024-11-20 15:38:25 +0800

    update

--- test/testJava/src/main/java/Form/TestForm/TestForm.java
+++ test/testJava/src/main/java/Form/TestForm/TestForm.java
@@ -1,10 +1,12 @@
+package Form.TestForm;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.border.*;
 /*
- * Created by JFormDesigner on Sun Nov 03 16:47:49 HKT 2024
+ * Created by JFormDesigner on Wed Nov 20 14:37:33 HKT 2024
  */
 
 
@@ -24,7 +26,7 @@ public class TestForm extends JFrame {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         ResourceBundle bundle = ResourceBundle.getBundle("form");
-        dialogPanel = new JPanel();
+        dialogPane = new JPanel();
         contentPanel = new JPanel();
         buttonBar = new JPanel();
         okButton = new JButton();
@@ -35,17 +37,15 @@ public class TestForm extends JFrame {
         var contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
 
-        //======== dialogPanel ========
+        //======== dialogPane ========
         {
-            dialogPanel.setBorder(new EmptyBorder(12, 12, 12, 12));
-            dialogPanel.setBackground(Color.white);
-            dialogPanel.setMinimumSize(new Dimension(1000, 700));
-            dialogPanel.setLayout(new BorderLayout());
+            dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+            dialogPane.setBackground(Color.white);
+            dialogPane.setLayout(new BorderLayout());
 
             //======== contentPanel ========
             {
                 contentPanel.setBackground(Color.white);
-                contentPanel.setMinimumSize(new Dimension(0, 0));
                 contentPanel.setLayout(null);
 
                 {
@@ -63,7 +63,7 @@ public class TestForm extends JFrame {
                     contentPanel.setPreferredSize(preferredSize);
                 }
             }
-            dialogPanel.add(contentPanel, BorderLayout.CENTER);
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
 
             //======== buttonBar ========
             {
@@ -75,7 +75,6 @@ public class TestForm extends JFrame {
 
                 //---- okButton ----
                 okButton.setText(bundle.getString("okButton.text"));
-                okButton.setFont(new Font("\u5fae\u8f6f\u96c5\u9ed1", Font.PLAIN, 12));
                 okButton.setBackground(Color.white);
                 buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
@@ -83,7 +82,6 @@ public class TestForm extends JFrame {
 
                 //---- cancelButton ----
                 cancelButton.setText(bundle.getString("cancelButton.text"));
-                cancelButton.setFont(new Font("\u5fae\u8f6f\u96c5\u9ed1", Font.PLAIN, 12));
                 cancelButton.setBackground(Color.white);
                 cancelButton.addMouseListener(new MouseAdapter() {
                     @Override
@@ -95,16 +93,16 @@ public class TestForm extends JFrame {
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH,
                     new Insets(0, 0, 0, 0), 0, 0));
             }
-            dialogPanel.add(buttonBar, BorderLayout.SOUTH);
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
         }
-        contentPane.add(dialogPanel, BorderLayout.CENTER);
+        contentPane.add(dialogPane, BorderLayout.CENTER);
         pack();
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
-    private JPanel dialogPanel;
+    private JPanel dialogPane;
     private JPanel contentPanel;
     private JPanel buttonBar;
     private JButton okButton;
--- test/testJava/src/main/java/Main.java
+++ test/testJava/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import Form.TestForm.TestForm;
+
 public class Main {
     public static void main(String[] args) {
         TestForm testForm = new TestForm();
