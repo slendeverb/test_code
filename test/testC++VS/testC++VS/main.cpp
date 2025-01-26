fe305dfaaee8108341fb6c15625a813b9607c5e5
stwings <571641990@qq.com>
2025-01-26 23:40:34 +0800

    update

--- test/testC++VS/testC++VS/main.cpp
+++ test/testC++VS/testC++VS/main.cpp
@@ -1,20 +1,5 @@
 import std;
-using namespace std;
-
-class Solution {
-public:
-	int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
-		int ans = 0;
-		if (a == e && !(a == c && abs(b - f) > abs(d - f)) || b == f && !(b == d && abs(a - e) > abs(c - e)) || abs(c - e) == abs(d - f) && !(e - a == a - c && f - b == b - d)) {
-			ans = 1;
-		}
-		else {
-			ans = 2;
-		}
-		return ans;
-	}
-};
 
 int main() {
-	std::cout << Solution{}.minMovesToCaptureTheQueen(4, 5, 7, 8, 2, 3) << "\n";
+
 }
